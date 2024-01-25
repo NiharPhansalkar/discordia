31dd376aaec71888624992ce45abcabafc49422a
Nihar Phansalkar <phansalkarnihar@gmail.com>
2024-01-25 14:20:29 +0000

    added sidebar to display invite, leave, etc

--- hooks/use-model-store.ts
+++ hooks/use-model-store.ts
@@ -1,17 +1,24 @@
+import { Server } from "@prism/client";
 import { create } from "zustand";
 
-export type ModelType = "createServer";
+export type ModelType = "createServer" | "invite";
+
+interface ModelData {
+  server?: Server;
+}
 
 interface ModelStore {
   type: ModelType | null;
+  data: ModelData;
   isOpen: boolean;
-  onOpen: (type: ModelType) => void;
+  onOpen: (type: ModelType, data?: ModelData) => void;
   onClose: () => void;
 }
 
 export const useModel = create<ModelStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
